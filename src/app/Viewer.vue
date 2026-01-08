0b03172e38dc9c956e2f191841e6876c7e2a722e
Lars Lorenz <mail@larslorenz.dev>
2026-01-08 21:52:15 +0100

    build fix

--- src/app/Viewer.vue
+++ src/app/Viewer.vue
@@ -1,15 +1,12 @@
 <template>
-  <div
-    ref="element"
-    class="container"
-  />
+  <div ref="element" class="container" />
 </template>
 
 <script setup lang="ts">
-import {storeToRefs} from "pinia";
-import {useModelStore} from "@/stores/model-store/modelStore.ts";
+import { storeToRefs } from "pinia";
+import { useModelStore } from "@/stores/model-store/modelStore.ts";
 
-const {element} = storeToRefs(useModelStore())
+storeToRefs(useModelStore())
 </script>
 
 <style lang="scss" scoped>
